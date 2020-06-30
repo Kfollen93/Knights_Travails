b52b4dbc241e832f9f19f0ba735068e45a0ea491
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-06-30 15:46:40 -0700

    Added notes for creating board

--- board.rb
+++ board.rb
@@ -7,6 +7,7 @@ class Board
     def create_board(board_array = [])
       square = Knight.new
       # Create an array that holds coordinates in a  8 x 8 grid
+      # p board_array = Array.new(8) {Array.new(8).each_with_index.map {|x,i| [0, i+1]} }
     end
 
     def create_rows
