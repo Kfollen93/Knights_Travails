734e2eec9ed708ceec7fe04e70710325a96957a7
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-06-30 12:25:24 -0700

    Added edge method

--- knight.rb
+++ knight.rb
@@ -1,4 +1,12 @@
 class Knight
-    def knight_moves
+    attr_reader :value
+  
+    def initialize(value)
+      @value = value
+      @adjacent_nodes = []
+    end
+    
+    def add_edge(adjacent_node)
+      @adjacent_nodes << adjacent_node
     end
 end
