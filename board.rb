c954a8b28d72b671b61504ec35102c6bd91d0564
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-07-01 09:51:58 -0700

    Created 8x8 grid with coordinates

--- board.rb
+++ board.rb
@@ -1,18 +1,15 @@
 class Board
-  
-    def initialize(board_array = [])
-    p  @board = create_board(board_array = [])
-    end
-    
-    def create_board(board_array = [])
-      square = Knight.new
-      # Create an array that holds coordinates in a  8 x 8 grid
-      # p board_array = Array.new(8) {Array.new(8).each_with_index.map {|x,i| [0, i+1]} }
+    attr_accessor :board
+    def initialize
+      @board = Array.new(8) { Array.new(8)} 
     end
 
-    def create_rows
-
+    def board_with_coordinates(board_array)
+      board_array.each_with_index.map do |row, row_index|
+        row.each_with_index.map do |column, column_index|
+          [row_index, column_index]
+        end
+      end
     end
-    
 end
  
