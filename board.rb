10915b4257baaaf1a68eb1d94fadaefce1bb5216
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-07-01 10:09:19 -0700

    Rubocop formatteD

--- board.rb
+++ board.rb
@@ -1,19 +1,18 @@
 class Board
-    attr_accessor :board
-    def initialize
-      @board = create_board
-    end
+  attr_accessor :board
+  def initialize
+    @board = create_board
+  end
 
-    def create_board
-      Array.new(8) { Array.new(8)}
-    end
+  def create_board
+    Array.new(8) { Array.new(8) }
+  end
 
-    def board_with_coordinates(board_array)
-      board_array.each_with_index.map do |row, row_index|
-        row.each_with_index.map do |column, column_index|
-          [row_index, column_index]
-        end
+  def board_with_coordinates(board_array)
+    board_array.each_with_index.map do |row, row_index|
+      row.each_with_index.map do |column, column_index|
+        [row_index, column_index]
       end
     end
+  end
 end
- 
