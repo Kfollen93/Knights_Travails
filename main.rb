63b4a4b739f8a1e17f3df4426152da874fa789c8
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-07-02 15:00:41 -0700

    Added additional file paths

--- main.rb
+++ main.rb
@@ -1,5 +1,7 @@
 require_relative 'board.rb'
 require_relative 'knight.rb'
+require_relative 'graph.rb'
+require_relative 'vertex.rb'
 
 p board = Board.new
 board_array = Array.new(8) { Array.new(8)}
