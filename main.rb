2d5def00646c70ed6ee2f24609f90ae43724b0f3
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-07-03 15:26:21 -0700

    Removed old array

--- main.rb
+++ main.rb
@@ -4,5 +4,4 @@ require_relative 'graph.rb'
 require_relative 'vertex.rb'
 
 p board = Board.new
-board_array = Array.new(8) { Array.new(8)}
 #board.board_with_coordinates(board_array) # for user to visualize
