c20da83c5e6ba244bb44c93f46696132444bfec1
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-07-06 16:16:10 -0700

    Added possible knight moves

--- knight.rb
+++ knight.rb
@@ -1,14 +1,23 @@
 class Knight
-    attr_accessor :row_start, :column_start, :row_end, :column_end
+    attr_accessor :row_start, :column_start, :row_end, :column_end, :x, :y
     def initialize
         @row_start = row_start
         @column_start = column_start
         @row_end = row_end
         @column_end = column_end
+        @x = x
+        @y = y
     end
 
     def knight_possible_moves
-        
+# write a method that finds all the moves a knight can make from a given square.
+# From that you can make your adjacency matrix — 
+# a square would be adjacent to another square if a knight could move
+# directly from one to the other.
+
+    x = [2, 1, -1, -2, -2, -1, 1, 2]
+    y = [1, 2, 2, 1, -1, -2, -2, -1]
+
     end
     
     def move_knight
