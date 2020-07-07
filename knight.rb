27afb29a4529375bfb99539c953516ac6c4d0e7e
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-07-07 14:59:25 -0700

    Added block for possible moves method

--- knight.rb
+++ knight.rb
@@ -1,12 +1,13 @@
 class Knight
-    attr_accessor :x, :y, :KNIGHT_MOVES, :location
+    attr_accessor :x, :y, :location, :moves
+    attr_reader :KNIGHT_MOVES
     def initialize
         @x = x
         @y = y
-        @KNIGHT_MOVES =
-         [[2, 1], [1, 2], [-1, 2], [-2, 1],
-         [-2, -1], [-1, -2], [1, -2], [2, -1]]
-        @location = location
+        @KNIGHT_MOVES = [[2, 1], [1, 2], [-1, 2], [-2, 1],
+        [-2, -1], [-1, -2], [1, -2], [2, -1]]
+        @location = [2,1]
+        @moves = moves
     end
 
     def knight_possible_moves
@@ -14,26 +15,12 @@ class Knight
 # From that you can make your adjacency matrix — 
 # a square would be adjacent to another square if a knight could move
 # directly from one to the other.
-    # find condition for each position where Knight moving is restricted
-    # knight position ex is [1][0]  - 1st array, first element
-
-    # if a Knight starts at 0th element in any array, it CANT MOVE:
-        # (-1, 2); (-1, -2); (-2, -1); (-2, 1)
-
-    # if a Knight starts in the 0th array && 0th element, it CANT MOVE:
-        # (-1, 2); (-1, -2); (-2, -1); (-2, 1); (1, 2); (2, 1)
-
-     # if a Knight starts in the 7th array (last) at any element, it CANT MOVE:
-        # (-1, -2); (1, -2); (-2, -1); (2, -1)
-    
-    # if a Knight starts in the 7th array (last) && 0th element, it CANT MOVE:
-        # (-1, 2); (-1, -2); (-2, -1); (-2, 1); (1, -2); (2, -1)
 
     x = location[0]
     y = location[1]
-    
+    moves = []
 
-
+    KNIGHT_MOVES.each { |move| moves << [x + move[0], y + move[1]] }
     end
     
     def move_knight
