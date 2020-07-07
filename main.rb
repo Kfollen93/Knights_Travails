72408dc5fc560f225df72c03e67c20ca88b4d2d9
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-07-07 14:59:40 -0700

    Added possible moves method to call

--- main.rb
+++ main.rb
@@ -5,5 +5,10 @@ require_relative 'vertex.rb'
 
 p board = Board.new
 board_array = Array.new(8) { Array.new(8) { Vertex.new } }
-board.board_with_coordinates(board_array) # for user to visualize
-p board.access_board
+# board.board_with_coordinates(board_array) # for user to visualize
+# board.access_board
+knight = Knight.new
+ KNIGHT_MOVES =
+ [[2, 1], [1, 2], [-1, 2], [-2, 1],
+ [-2, -1], [-1, -2], [1, -2], [2, -1]]
+print knight.knight_possible_moves
