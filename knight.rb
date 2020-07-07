0e9afc7f157c8de5f57d1d46670e4aacdefef7d9
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-07-07 15:47:24 -0700

    Refactored possibles_moves method

--- knight.rb
+++ knight.rb
@@ -1,22 +1,18 @@
 class Knight
-    attr_accessor :x, :y, :location, :moves, :KNIGHT_MOVES
-    #attr_reader :KNIGHT_MOVES
-    def initialize
-        @x = x
-        @y = y
-        @KNIGHT_MOVES = [[2, 1], [1, 2], [-1, 2], [-2, 1],
-        [-2, -1], [-1, -2], [1, -2], [2, -1]]
-        @location = [3,3]
-        @moves = moves
+    attr_accessor :x, :y
+
+    @KNIGHT_MOVES = [[2, 1], [1, 2], [-1, 2], [-2, 1],
+    [-2, -1], [-1, -2], [1, -2], [2, -1]]
+
+    def initialize(location)
+        @x = location[0]
+        @y = location[1]
     end
 
     def knight_possible_moves
-    x = location[0]
-    y = location[1]
     moves = []
-
     KNIGHT_MOVES.each { |move| moves << [x + move[0], y + move[1]] }
-    moves
+   p moves
     end
     
     def move_knight
