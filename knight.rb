79fd0074f4702d2b6823622c6bd4b21c68541ea7
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-07-08 08:50:54 -0700

    Fixed formatting

--- knight.rb
+++ knight.rb
@@ -1,19 +1,18 @@
 class Knight
-    KNIGHT_MOVES = [[2, 1], [1, 2], [-1, 2], [-2, 1],
-    [-2, -1], [-1, -2], [1, -2], [2, -1]]
+  KNIGHT_MOVES = [[2, 1], [1, 2], [-1, 2], [-2, 1],
+                  [-2, -1], [-1, -2], [1, -2], [2, -1]]
 
-    def valid_moves(location)
-    p    possible_moves(location).select { |move| move[0].between?(0, Board::MAX) && move[1].between?(0, Board::MAX) }
-    end
+  def valid_moves(location)
+    p possible_moves(location).select { |move| move[0].between?(0, Board::MAX) && move[1].between?(0, Board::MAX) }
+  end
 
-     private
+  private
 
-    def possible_moves(location)
+  def possible_moves(location)
     x = location[0]
     y = location[1]
     moves = []
     KNIGHT_MOVES.each { |move| moves << [x + move[0], y + move[1]] }
     moves
-    end
-     
+  end
 end
