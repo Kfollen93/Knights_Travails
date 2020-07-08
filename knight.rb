e4a89cc41f9c41f063ef366c258f0523724a8cf5
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-07-08 10:15:02 -0700

    Added board comment

--- knight.rb
+++ knight.rb
@@ -2,7 +2,7 @@ class Knight
   KNIGHT_MOVES = [[2, 1], [1, 2], [-1, 2], [-2, 1],
                   [-2, -1], [-1, -2], [1, -2], [2, -1]]
 
-  def valid_moves(location)
+  def valid_moves(location) # Any move that is negative or above 7 would be outside the array (off the board, hence omitted)
     possible_moves(location).select { |move| move[0].between?(0, Board::MAX) && move[1].between?(0, Board::MAX) }
   end
 
