557e52905bf3aa737eeaf4476b372fec96ff5142
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-07-08 11:33:50 -0700

    Removed comments

--- main.rb
+++ main.rb
@@ -5,9 +5,5 @@ require_relative 'vertex.rb'
 
 p board = Board.new
 board_array = Array.new(8) { Array.new(8) { Vertex.new } }
-# board.board_with_coordinates(board_array) # for user to visualize
-# board.access_board
 knight = Knight.new
-print knight.possible_moves([0,3])
-puts ""
-print knight.valid_moves([0,3])
+print knight.valid_moves([7,7])
