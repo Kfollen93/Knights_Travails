53a8077af82435fef87a77dfdb34576b997bb493
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-07-09 08:21:18 -0700

    Added key and value vars

--- vertex.rb
+++ vertex.rb
@@ -1,6 +1,8 @@
 class Vertex
-    attr_accessor :key, :edge
+    attr_accessor :key, :edge, :value
     def initialize
         @edge = edge
+        @key = key
+        @value = value
     end
 end
