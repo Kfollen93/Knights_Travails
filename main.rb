298b1c60f3f59e7cdbfad6b79714a8c7add20c01
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-07-10 17:07:00 -0700

    Added method to driver script

--- main.rb
+++ main.rb
@@ -3,10 +3,8 @@ require_relative 'knight.rb'
 require_relative 'graph.rb'
 require_relative 'vertex.rb'
 
-board = Board.new
 board_array = Array.new(8) { Array.new(8) { Vertex.new } }
+board = Board.new
 knight = Knight.new
 knight.valid_moves([0,0])
-
 p board.board_coordinates
-
