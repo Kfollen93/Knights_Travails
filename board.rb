950475d09c65e962e023392e5c7c6d914b55c3c1
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-07-10 17:05:12 -0700

    Added board_coordinates logic

--- board.rb
+++ board.rb
@@ -3,21 +3,19 @@
 # Creates 8x8 grid
 class Board
   attr_accessor :board_array
-  attr_reader :letters, :numbers
+
+  LETTERS = ["A", "B", "C", "D", "E", "F", "G", "H"]
+  NUMBERS = ["1", "2", "3", "4", "5", "6", "7", "8"]
   MAX = 7
 
   def initialize
     @board_array = Array.new(8) { Array.new(8) { Vertex.new } }
-    @letters = ["A", "B", "C", "D", "E", "F", "G", "H"]
-    @numbers = ["1", "2", "3", "4", "5", "6", "7", "8"]
+    board_coordinates
   end
 
   def board_coordinates
-    letters.product(numbers).zip(board_array) do |cords, _|
-      cords.join # need to figure how to set each @key equal to one coordinate.
-      end
+    LETTERS.map{ |letter| [letter].product(NUMBERS).map(&:join) }
   end
-
 
 end
 
