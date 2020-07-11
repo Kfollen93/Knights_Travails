8dd8307bde95d9e11761e0798df208255002ae70
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-07-11 14:17:30 -0700

    Added map_coordinates holder

--- vertex.rb
+++ vertex.rb
@@ -8,11 +8,16 @@ class Vertex
         @edge = edge
         @key = key
         @value = value
-        @coordinates = board_coordinates # see below comment
+        @coordinates = coordinates # see below comment
     end
     
     def board_coordinates
         # Figure out how to get one cord for each spot in board_array
         LETTERS.map { |letter| [letter].product(NUMBERS).map(&:join) }
     end
+
+    def map_coordinates
+        
+    end
+
 end
