49d67b0902351a6946281ad17d2bdb14aa150d84
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-07-10 17:59:13 -0700

    Removed constants

--- board.rb
+++ board.rb
@@ -4,18 +4,14 @@
 class Board
   attr_accessor :board_array
 
-  LETTERS = ["A", "B", "C", "D", "E", "F", "G", "H"]
-  NUMBERS = ["1", "2", "3", "4", "5", "6", "7", "8"]
+
   MAX = 7
 
   def initialize
     @board_array = Array.new(8) { Array.new(8) { Vertex.new } }
-    board_coordinates
   end
 
-  def board_coordinates
-    LETTERS.map{ |letter| [letter].product(NUMBERS).map(&:join) }
-  end
+
 
 end
 
