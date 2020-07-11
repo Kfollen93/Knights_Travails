3a1ba23596a6ad14d8d0189df9b600e050dbdacb
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-07-10 18:00:01 -0700

    Removed print from driver

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ require_relative 'graph.rb'
 require_relative 'vertex.rb'
 
 board_array = Array.new(8) { Array.new(8) { Vertex.new } }
-board = Board.new
+p board = Board.new
 knight = Knight.new
 knight.valid_moves([0,0])
-p board.board_coordinates
+
