7e251eb7492b8876043821e70b0654ff3f572bea
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-07-10 17:59:27 -0700

    Added constants

--- vertex.rb
+++ vertex.rb
@@ -1,8 +1,18 @@
 class Vertex
-    attr_accessor :key, :edge, :value
+    attr_accessor :key, :edge, :value, :coordinates, :LETTERS, :NUMBERS
+
+    LETTERS = ["A", "B", "C", "D", "E", "F", "G", "H"]
+    NUMBERS = ["1", "2", "3", "4", "5", "6", "7", "8"]
+
     def initialize
         @edge = edge
         @key = key
         @value = value
+        @coordinates = board_coordinates # see below comment
+    end
+    
+    def board_coordinates
+        # Figure out how to get one cord for each spot in board_array
+        LETTERS.map { |letter| [letter].product(NUMBERS).map(&:join) }
     end
 end
