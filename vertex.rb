ddd109fd5302ccf1d752e26784b38bd78e8a1793
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-07-12 15:01:34 -0700

    Added board_coordinates method to coordinates

--- vertex.rb
+++ vertex.rb
@@ -1,5 +1,5 @@
 class Vertex
-    attr_accessor :key, :edge, :value, :coordinates, :LETTERS, :NUMBERS
+    attr_accessor :key, :edge, :value, :coordinates, :LETTERS, :NUMBERS, :cords
 
     LETTERS = ["A", "B", "C", "D", "E", "F", "G", "H"]
     NUMBERS = ["1", "2", "3", "4", "5", "6", "7", "8"]
@@ -8,10 +8,10 @@ class Vertex
         @edge = edge
         @key = key
         @value = value
-        @coordinates = coordinates # see below comment
+        @coordinates = board_coordinates # see below comment
     end
     
-    def board_coordinates
+    def board_coordinates(cords = @cords)
         # Figure out how to get one cord for each spot in board_array
         LETTERS.map { |letter| [letter].product(NUMBERS).map(&:join) }
     end
