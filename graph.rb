e304a33d30935f160538de5e5fd39631a1da8cc5
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-07-14 17:04:31 -0700

    Added hash_moves method

--- graph.rb
+++ graph.rb
@@ -1,4 +1,9 @@
 class Graph
-    def adj_hash
-    end
+
+def hash_moves 
+    adj_hash = {}
+    (0..7).to_a.repeated_permutation(2).to_a.each { |k| adj_hash[k] = valid_moves(k) }
+    adj_hash
+end
+
 end
